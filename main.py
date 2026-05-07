1c12fbd4a964cd1b05144b28b4bebf95037470f2
vempallicharishma <vempallicharishma@gmail.com>
2026-05-07 10:56:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+IMG_SIZE = (150, 150)
+BATCH_SIZE = 8
+
+train_gen = ImageDataGenerator(rescale=1./255)
+val_gen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_gen.flow_from_directory(
+    'data/train',
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='binary'
+)
+
+val_data = val_gen.flow_from_directory(
+    'data/validation',
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='binary'
+)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+model.fit(
+    train_data,
+    epochs=5,
+    validation_data=val_data
+)
+
+model.save('model/dog_cat_model.h5')
+
